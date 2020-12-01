1fedb23b87aca84243a145aa11938774a00ee146
Tr00y2ooo <47392486+Impossibleimagine@users.noreply.github.com>
2020-12-02 01:34:10 +0800

    Add files via upload

---
+++ DDoS.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+    int b;
+    char d;
+    string a,c;
+    cin.tie(0);
+    ios::sync_with_stdio;
+    cout<<"歡贏使用DDoS"<<endl<<endl;
+    cout<<"1.輸入IP"<<endl<<endl;
+    cout<<"2.輸入攻擊強度(1~999999999)"<<endl<<endl;
+    cout<<"1."<<endl;
+    cout<<">>";
+    getline(cin,a);
+    cout<<endl<<"水!"<<endl;
+    Sleep(1000);
+    cout<<"之!"<<endl;
+    Sleep(1000);
+    cout<<"呼!"<<endl;
+    Sleep(1000);
+    cout<<"麻!"<<endl;
+    Sleep(1000);
+    cout<<endl;
+    cout<<"進行連線中..."<<endl<<endl;
+    //
+	printf("[llllll");
+	Sleep(5000);
+	printf("llllll");
+	Sleep(2000);
+	printf("llllll");
+	Sleep(3000);
+	printf("llllll");
+	Sleep(4000);
+	printf("llllll");
+	Sleep(1000);
+	printf("llllll");
+	Sleep(3000);
+	printf("llllll");
+	Sleep(2000);
+	printf("llllll]\n");
+	Sleep(3000);
+    cout<<endl<<"就緒[v]"<<endl;
+    cout<<endl;
+    cout<<"2."<<endl;
+    cout<<">>";
+    cin>>b;
+    cout<<"確定使用"<<b<<"強度進行攻擊?"<<endl;
+    cout<<"[Y/N]"<<endl;
+    cin>>d;
+    if(d=='N'){
+        cout<<"攻擊中止 :)"<<endl;
+        return(0);
+    }
+    Sleep(2000);
+    cout<<endl<<"建立連線中..."<<endl;
+    Sleep(5000);
+    cout<<endl<<"attacking ^_^ "<<endl<<endl;
+    Sleep(1000);
+    while(b--){
+        Sleep(100);
+    cout<<"正在對"<<a<<"發送32位元組封包>>>>>"<<endl;
+    }
+    cout<<endl<<"攻擊完成~"<<endl<<endl<<"Author By.Hacker T"<<endl;
+    system("pause");
+
+
+}
+
